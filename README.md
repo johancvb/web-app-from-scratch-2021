0e8fac7200e0d49f20e7738e1a613a5334a4aa60
johancvb <35685184+johancvb@users.noreply.github.com>
2021-02-04 22:15:31 +0100

    overview page gemaakt, nog lang niet af, uren lang met API gekloot, werkt nog niet goed

--- README.md
+++ README.md
@@ -22,6 +22,34 @@ Goal: Retrieve data from an API and render it in an overview page.
 
 [Slides](https://docs.google.com/presentation/d/1hXfgIhBzJ2QXDmpKpMcx7G-a9R_fVrcPdeOyM3G0I00/edit?usp=sharing)
 
+
+# ReadMe week 1 #
+
+---Hier een link naar mijn opdracht:---
+https://johancvb.github.io/web-app-from-scratch-2021/
+
+---Wat doet mijn app en wat is het doel?---
+Mijn app laat verschillende informatie zijn van alle NBA spelers die ooit actief zijn geweest in de NBA.
+
+---Welke actors zitten in mijn app?---
+De actors in mijn app zijn de spelers zelf. Hun data wordt laten zien op de overview page.
+
+---Van welke API maak ik gebruik?---
+Ik maak gebruik van de API: Free NBA van theapiguy (https://rapidapi.com/theapiguy/api/free-nba/details)
+
+---Hoe zit het met interactie in mijn app?---
+Ik wil mijn app het de gebruiker gemakkelijk maken, door bijvoorbeeld een overzichtelijke style te gebruiken, 
+en de gebruiker duidelijk te vragen/zeggen wat te doen.
+
+---Welk design ga ik gebruiken?---
+Ik wil op mijn applicatie een scroll overzicht hebben van de spelers, (misschien nog met filters), en als je op de speler klikt,
+komt er een detailed page popup die alle informatie van die speler laat zien.
+
+---Wat wil ik nog toevoegen?---
+Aangezien ik nog nooit met API's heb gewerkt lijkt het me qua tijd verstandig om het niet te overdreven te maken. 
+Maar als ik tijd genoeg heb, wil ik nog filters toepassen, en een zoekbalk.
+Ook zou ik graag nog een API willen koppelenn met mijn app, zodat er per speler ook nog een foto van de speler tevoorschijn komt.
+
 ### Week 2 - Design and Refactor 🛠
 
 Goal: Design the web app. Add routes and states. Split code into modules. Rendering detail page.
--- app.js
+++ app.js
@@ -2,36 +2,61 @@ const player_name = document.getElementById('name');
 const team = document.getElementById('team');
 const position = document.getElementById('position');
 const player_height = document.getElementById('player_height');
-
-
-
 const player_btn = document.getElementById('player_btn');
 
+let id_nr = 1
 
-player_btn.addEventListener('click', function(){
+async function loopAPI(){
     
-    const rand_int = Math.floor(Math.random() * 100)
-
-    fetch(`https://free-nba.p.rapidapi.com/players/${rand_int}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "f13b636f5fmshaea25718ef36c73p1829b0jsncd19791a2844",
-		"x-rapidapi-host": "free-nba.p.rapidapi.com"
-	} 
-})
-.then(res => res.json()
-)
-.then(data => {
-    console.log(data)
-    player_name.innerHTML = `<p>${data.first_name} ${data.last_name}</p>`
-    team.innerHTML = `<p>${data.team.full_name}</p>`
-    position.innerHTML = `<p>${data.position}</p>`
-    player_height.innerHTML = `<p>${data.height_feet}'${data.height_inches}</p>`
-    if(data.height_feet === null || data.height_inches === null) {
-        player_height.innerHTML = "<p>This info is unavailable.</p>"
+    for(i = 0; i < 50; i++) {
+        fetch(`https://free-nba.p.rapidapi.com/players/${id_nr}/?page=0&per_page=30`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "f13b636f5fmshaea25718ef36c73p1829b0jsncd19791a2844",
+                "x-rapidapi-host": "free-nba.p.rapidapi.com"
+            }
+        })
+        .then(res => res.json()
+        )
+        .then(data => {
+            console.log(data)
+              //  player_name.innerHTML = `<p>${data.first_name} ${data.last_name}</p>`
+                  player_name.insertAdjacentHTML("beforeend", `<p>${data.first_name} ${data.last_name}</p>`)
+            } 
+        )   
+        id_nr++  
     }
+}
+
+loopAPI()
 
 
-})
-console.log(rand_int)
-});
+
+
+// player_btn.addEventListener('click', function(){
+    
+//     const rand_int = Math.floor(Math.random() * 100)
+
+//     fetch(`https://free-nba.p.rapidapi.com/players/${rand_int}`, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "f13b636f5fmshaea25718ef36c73p1829b0jsncd19791a2844",
+// 		"x-rapidapi-host": "free-nba.p.rapidapi.com"
+// 	} 
+// })
+// .then(res => res.json()
+// )
+// .then(data => {
+//     console.log(data)
+//     player_name.innerHTML = `<p>${data.first_name} ${data.last_name}</p>`
+//     team.innerHTML = `<p>${data.team.full_name}</p>`
+//     position.innerHTML = `<p>${data.position}</p>`
+//     player_height.innerHTML = `<p>${data.height_feet}'${data.height_inches}</p>`
+//     if(data.height_feet === null || data.height_inches === null) {
+//         player_height.innerHTML = "<p>This info is unavailable.</p>"
+//     }
+
+
+// })
+// console.log(rand_int)
+// });
